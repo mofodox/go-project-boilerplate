f300f77c795c026834461883222eaac5058f24e6
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-07-07 22:31:32 +0800

    Initialise Fiber instance

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	app.Get("/", home)
+
+	app.Listen(":3000")
+}
+
+func home(c *fiber.Ctx) error {
+	return c.SendString("Hello World!")
 }
